418f5b521ace39cd586fb83107a136861fbae4ae
Zac Hobday <zachobday0@gmail.com>
2023-12-21 00:11:17 +0000

    update camera feed to correct image capture sizing. Capture frame still seems stretched but is in full screen size.

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,10 @@
 import React, { useEffect, useRef, useState } from 'react';
 import * as tf from '@tensorflow/tfjs';
 import '@tensorflow/tfjs';
+import { set } from 'mongoose';
 
 const PoseDetectionComponent = () => {
     const videoRef = useRef<HTMLVideoElement | null>(null);
-    const [model, setModel] = useState<tf.GraphModel | null>(null);
     const cr = useRef<HTMLCanvasElement | null>(null);
     const [poses, setPoses] = useState<any[]>([]);
     const [capturedImageSrc, setCapturedImageSrc] = useState<string | null>(null);
@@ -13,16 +13,48 @@ const PoseDetectionComponent = () => {
     // Load the model when the component mounts
     const loadModel = async () => {
         await tf.ready();
-        const modelPath = 'http://localhost:5000/multipose-lightning';
-        const loadedModel = await tf.loadGraphModel(`${modelPath}/model.json`);
+        const modelPath = 'http://localhost:5000/multipose-lightning/model.json';
+        const loadedModel = await tf.loadGraphModel(modelPath);
+      
         if (loadedModel == null) {
-            return;
-          }else{
-              setModel(loadedModel);
-              console.log(model);
+          return;
+        } else {
+          const intervalId = setInterval(() => {
+            const canvas = cr.current;
+            const video = videoRef.current;
+      
+            if (!video || !canvas) {
               return;
-          }
-    };
+            }
+      
+            const context = canvas.getContext('2d');
+      
+            if (!context) {
+              return;
+            }
+      
+            // Capture frame
+            context.drawImage(video, 0, 0, canvas.width, canvas.height);
+            const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+            const dataUrl = canvas.toDataURL('image/png');
+            setCapturedImageSrc(dataUrl);
+
+            // Do something with imageData (e.g., log it)
+            console.log('Captured frame:', imageData);
+      
+          }, 1000); // Interval in milliseconds (1000ms = 1 second)
+      
+          // Optionally, store the intervalId in state or elsewhere for cleanup
+          // Cleanup example: clearInterval(intervalId);
+      
+          // Return cleanup function to clear the interval when needed
+          return () => {
+            clearInterval(intervalId);
+          };
+        }
+      };
+      
 
     // Initialize the camera and load the model
     const setupCamera = async () => {
@@ -38,53 +70,18 @@ const PoseDetectionComponent = () => {
     };
 
     const calculateSize = () => {
-        const windowWidth = window.innerWidth;
-        const windowHeight = window.innerHeight;
-  
-        // Ensure both width and height are multiples of 32
-        const maxWidth = Math.floor(windowWidth / 32) * 32;
-        const maxHeight = Math.floor(windowHeight / 32) * 32;
-  
-        // Set video and canvas size
-        if (videoRef.current) {
-          videoRef.current.width = maxWidth;
-          videoRef.current.height = maxHeight;
-        }
-  
-        if (cr.current) {
-          cr.current.width = maxWidth;
-          cr.current.height = maxHeight;
-        }
+          if (cr.current) {
+            // Set the width and height properties correctly based on videoRef
+            cr.current.width = videoRef.current?.width || 0;
+            cr.current.height = videoRef.current?.height || 0;
+          }
       };
-
-      const detectPoses = () => {
-        const canvas = cr.current;
-        const video = videoRef.current;
-     
-        if (!video || !canvas) {return;}
-
-        const context = canvas.getContext('2d');
-
-        if (!context) {return;}
-
-        // Get image data from the canvas
-        context.drawImage(video,0,0,canvas.width,canvas.height)
-        const imageData = context.getImageData(0, 0, canvas.width, canvas.width);
-        const pixelData = imageData.data;
-        const dataUrl = canvas.toDataURL('image/png');
-        setCapturedImageSrc(dataUrl);
-
-      }
 
       // Call calculateSize initially and add a resize event listener
       const setupAndDetect = async() => {
         await calculateSize();
         await setupCamera();
         await loadModel();
-        const intervalId = setInterval(() => detectPoses(),1000);
-        return () =>{
-          clearInterval(intervalId)
-        };
       }
       setupAndDetect();
       
@@ -100,7 +97,7 @@ const PoseDetectionComponent = () => {
   return (
     <div>
         <div>
-            <video ref={videoRef} autoPlay playsInline muted />
+            <video ref={videoRef} style={{ width: '100%', height: '100vh', position: 'relative' }} autoPlay  muted />
             <canvas ref={cr} />
         </div>
         {capturedImageSrc && (
--- src/CameraFeed.tsx
+++ src/CameraFeed.tsx
@@ -1,7 +1,13 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useEffect, useRef,useState } from 'react';
+import * as tf from '@tensorflow/tfjs';
+
 
 const CameraFeed: React.FC = () => {
   const videoRef = useRef<HTMLVideoElement>(null);
+  const cr = useRef<HTMLCanvasElement | null>(null);
+  const [capturedImageSrc, setCapturedImageSrc] = useState<string | null>(null);
+
+  
 
   useEffect(() => {
     const initCamera = async () => {
@@ -19,7 +25,79 @@ const CameraFeed: React.FC = () => {
     };
 
     initCamera();
+    loadModel();
   }, []);
+
+  const handleResize = () => {
+    if (videoRef.current && cr.current) {
+      const windowWidth = window.innerWidth;
+      const windowHeight = window.innerHeight;
+
+      // Ensure both width and height are multiples of 32
+      const maxWidth = Math.floor(windowWidth / 32) * 32;
+      const maxHeight = Math.floor(windowHeight / 32) * 32;
+
+      // Set video element dimensions
+      videoRef.current.width = maxWidth;
+      videoRef.current.height = maxHeight;
+
+      // Set canvas dimensions
+      cr.current.width = maxWidth;
+      cr.current.height = maxHeight;
+    }
+  };
+
+  // Listen for window resize events
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+    handleResize(); // Call on initial mount
+
+    // Cleanup the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
+  const loadModel = async () => {
+    await tf.ready();
+    const modelPath = 'http://localhost:5000/multipose-lightning/model.json';
+    const loadedModel = await tf.loadGraphModel(modelPath);
+    
+    if (loadedModel == null) {
+      return;
+    } else {
+      const intervalId = setInterval(() => {
+        const canvas = cr.current;
+        const video = videoRef.current;
+  
+        if (!video || !canvas) {
+          return;
+        }
+  
+        const context = canvas.getContext('2d');
+  
+        if (!context) {
+          return;
+        }
+  
+        // Capture frame
+        context.drawImage(video, 0, 0, canvas.width, canvas.height);
+        const imageData = context.getImageData(0, 0, canvas.width, canvas.height);
+
+        const dataUrl = canvas.toDataURL('image/png');
+        setCapturedImageSrc(dataUrl);
+
+        // Do something with imageData (e.g., log it)
+        console.log('Captured frame:', imageData);
+  
+      }, 1000); // Interval in milliseconds (1000ms = 1 second)
+  
+      // Return cleanup function to clear the interval when needed
+      return () => {
+        clearInterval(intervalId);
+      };
+    }
+  };  
 
   return (
     <div style={{ width: '100%', height: '100vh', position: 'relative' }}>
@@ -31,6 +109,7 @@ const CameraFeed: React.FC = () => {
         playsInline
         muted
       ></video>
+      <canvas ref={cr} />
       <div
         style={{
           position: 'absolute',
@@ -45,6 +124,12 @@ const CameraFeed: React.FC = () => {
         <button onClick={() => console.log('Button Clicked')}>Function 1</button>
         <button onClick={() => console.log('Button Clicked')}>Function 2</button>
       </div>
+      {capturedImageSrc && (
+        <div>
+          <h2>Captured Image</h2>
+          <img src={capturedImageSrc} alt="Captured Frame" />
+        </div>
+      )}
     </div>
   );
 };
